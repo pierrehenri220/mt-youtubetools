dfe697c81425b4deb031a376a3f49b0a7fb644a6
Pierre-Henri Lavigne <peter@colsis.jp>
2018-12-28 02:05:10 +0900

    正規表現の調整

--- lib/YoutubeTools/Callbacks.pm
+++ lib/YoutubeTools/Callbacks.pm
@@ -5,12 +5,12 @@ use strict;
 sub _build_page {
     my ($cb, %args) = @_;
     my $content = ${$args{'Content'}};
-    if ($content =~ /\[youtube https:\/\/www\.youtube\.com\/.*?list\=\w+\]/g) {
-        $content =~ s/\[youtube https:\/\/www\.youtube\.com\/.*?list\=(\w+)\]/<div class\=\"asset-video youtube-asset"><iframe width\=\"560\" height\=\"315\" src\=\"https\:\/\/www\.youtube-nocookie\.com\/embed\/videoseries\?list=$1&rel\=0\" frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen><\/iframe><\/div>/g;
+    if ($content =~ /\[youtube https:\/\/www\.youtube\.com\/.*?list\=[\w+-_]+\]/g) {
+        $content =~ s/\[youtube https:\/\/www\.youtube\.com\/.*?list\=([\w+-_]+)\]/<div class\=\"asset-video youtube-asset"><iframe width\=\"560\" height\=\"315\" src\=\"https\:\/\/www\.youtube-nocookie\.com\/embed\/videoseries\?list=$1&rel\=0\" frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen><\/iframe><\/div>/g;
         ${$args{'Content'}} = $content;
     }
-    if ($content =~ /\[youtube https:\/\/www\.youtube\.com\/watch\?v\=\w+-\]/g) {
-        $content =~ s/\[youtube https:\/\/www\.youtube\.com\/watch\?v\=(\w+-)\]/<div class\=\"asset-video youtube-asset"><iframe width\=\"560\" height\=\"315\" src\=\"https\:\/\/www\.youtube-nocookie\.com\/embed\/$1\?rel\=0\" frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen><\/iframe><\/div>/g;
+    if ($content =~ /\[youtube https:\/\/www\.youtube\.com\/watch\?v\=[\w+-_]+\]/g) {
+        $content =~ s/\[youtube https:\/\/www\.youtube\.com\/watch\?v\=([\w+-_]+)\]/<div class\=\"asset-video youtube-asset"><iframe width\=\"560\" height\=\"315\" src\=\"https\:\/\/www\.youtube-nocookie\.com\/embed\/$1\?rel\=0\" frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen><\/iframe><\/div>/g;
         ${$args{'Content'}} = $content;
     }
 }
